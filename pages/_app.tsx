34ccbf1aedd7d11a3237e5180cb3a6dc4ac10b12
musclesandtakos <musclesandtakos@gmail.com>
2026-01-04 20:19:57 +0000

    Add CodeRabbit configuration and fix build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
